af833d0a0440afda540c5c00b066d4eaca71d940
annarafferty <arafferty@carleton.edu>
2020-10-14 11:21:35 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+    /**
+     * From codingbat: https://codingbat.com/prob/p107330
+     * We have bunnies standing in a line, numbered 1, 2, ... 
+     * The odd bunnies (1, 3, ..) have the normal 2 ears. The 
+     * even bunnies (2, 4, ..) we'll say have 3 ears, because 
+     * they each have a raised foot. Recursively return the 
+     * number of "ears" in the bunny line 1, 2, ... n (without 
+     * loops or multiplication).
+     * Example input and expected output:
+     *    bunnyEars2(0) → 0
+     *    bunnyEars2(1) → 2
+     *    bunnyEars2(2) → 5
+     * You can copy and paste your code into codingbat
+     * to test further. Note that on codingbat, the
+     * method must be non-static rather than static.
+     * Nothing else about your implementation needs to
+     * change to test it there.
+     */
+    public static int bunnyEars2(int bunnies) {
+        return 0;//Delete this line and replace with your implementation!
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(Integer.parseInt(args[0])));
+    }
+}
